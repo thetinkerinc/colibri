99c07207f9f428c8668eb727e9ef930c3fecc3bd
Shane Brass <shanebrass1618@gmail.com>
2023-09-04 17:50:37 -0600

    Removing sanitizer

--- src/lib/components/html.svelte
+++ src/lib/components/html.svelte
@@ -1,13 +1,6 @@
 <script>
 export let html;
-
-import * as dompurify from 'isomorphic-dompurify';
-
-$: clean = dompurify.sanitize(html, {
-	FORCE_BODY: true,
-	ALLOWED_ATTR: ['style', 'class', 'type', 'href', 'rel']
-});
 </script>
 
 <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-{@html clean}
+{@html html}
