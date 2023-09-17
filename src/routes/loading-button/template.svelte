f85c471c665acf3882f7ee6801a431212b57c8fa
Shane Brass <shanebrass1618@gmail.com>
2023-09-17 17:51:41 -0600

    Adding LoadingButton component

---
+++ src/routes/loading-button/template.svelte
@@ -0,0 +1,25 @@
+<script>
+import { LoadingButton } from '@thetinkerinc/colibri';
+
+import style from './style.js';
+
+let beer;
+
+async function handleClick() {
+	const resp = await fetch(
+		'https://random-data-api.com/api/v2/beers'
+	);
+	const json = await resp.json();
+	beer = json;
+}
+</script>
+
+<!-- prettier-ignore -->
+<LoadingButton [[props]] {style} action={handleClick}>[[slot-default]] [[slot-confirm]]</LoadingButton>
+{#if beer}
+	<div>
+		You should try
+		{beer.brand}
+		{beer.name}
+	</div>
+{/if}
